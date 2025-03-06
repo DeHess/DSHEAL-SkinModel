22cb5bc16d846efd025cc228ee4b8aee381f2817
DeHess <nathhess@gmail.com>
2025-03-06 11:11:25 +0100

    import tensorflow

--- classification.py
+++ classification.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras import layers, models
 
 image_folder = "archive/ISIC_2019_Training_Input"
 metadata_csv = "archive/ISIC_2019_Training_Metadata.csv"
@@ -21,15 +23,18 @@ metadata["sex"] = metadata["sex"].fillna("unknown")
 # Use the median age if no age is specified 
 metadata["age_approx"] = metadata["age_approx"].fillna(metadata["age_approx"].median())
 
+
+"""
 # Merge metadata with ground truth using "image" as the key
 df = pd.merge(metadata, groundtruth, on="image")
 
 print(df.columns)
 
-train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
 
 print("Training data sample:")
 print(train_df.head())
 
 print("Testing data sample:")
-print(test_df.head())
+print(test_df.head())"
+"""
